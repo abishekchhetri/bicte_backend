d932d2d231d4210a99da06c93e8d25af752621fa
jivani-sapkota <sapkotajiwani6@gmail.com>
2025-07-04 09:49:56 +0545

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret_here';
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const protect = (roles = []) => {
   if (typeof roles === 'string') roles = [roles];
